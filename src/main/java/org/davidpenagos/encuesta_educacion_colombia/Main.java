0818e47a987664381c42bb0462412f8e01afb252
David <david.penagos1407@gmail.com>
2023-10-17 22:43:19 -0500

    Estructura inicial

--- src/main/java/org/davidpenagos/encuesta_educacion_colombia/Main.java
+++ src/main/java/org/davidpenagos/encuesta_educacion_colombia/Main.java
@@ -2,6 +2,7 @@ package org.davidpenagos.encuesta_educacion_colombia;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Encuesta[] datosEncuesta = Encuesta.recogerDatos();
+        Estadisticas.mostrarEstadisticas(datosEncuesta);
     }
 }
